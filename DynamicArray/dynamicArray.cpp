d02b402981f76aabc4c7aac8fdb3813cc699f0f6
Alexandre Duclos <bass_player96@hotmail.com>
2015-02-03 08:02:58 -0500

    bloqué à setElement

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -5,13 +5,16 @@
 DynamicArray::DynamicArray(){
 	capacite = 100;
 	tabElements = new int[capacite];
-	for (int i = 0; i > capacite; i++){
+	for (int i = 0; i < capacite; i++){
 		tabElements[i] = 0;
 	} 
 }
 
 DynamicArray::DynamicArray(unsigned int _capacite){
 	capacite = _capacite;
+	for (int i = 0; i < capacite; i++){
+		tabElements[i] = 0;
+	}
 }
 
 DynamicArray::~DynamicArray(){
@@ -23,7 +26,16 @@ unsigned int DynamicArray::getCapacite(){
 }
 
 void DynamicArray::setCapacite(unsigned int _capacite){
-
+	int* nouvelleTable = new int[_capacite]; 
+	for (int i = 0; i < _capacite; i++){
+		nouvelleTable[i] = 0;
+	}
+	for (int i = 0; i < capacite; i++) {
+		nouvelleTable[i] = tabElements[i];
+	}
+	capacite = _capacite;
+	tabElements = nouvelleTable;
+	
 }
 
 int DynamicArray::getElement(unsigned int _index){
@@ -31,6 +43,6 @@ int DynamicArray::getElement(unsigned int _index){
 }
 
 void DynamicArray::setElement(unsigned int _index, int _valeur){
-
+	tabElements[_index] = _valeur;
 }
 
--- DynamicArrayTests/DynamicArrayTests.cpp
+++ DynamicArrayTests/DynamicArrayTests.cpp
@@ -41,46 +41,46 @@ namespace DynamicArrayTests
 		}
 
 
-		//TEST_METHOD(setElement_devrait_initialiser_un_element_du_dynamicArray)
-		//{
-		//	//Arrange
-		//	const int NOUVELLE_VALEUR_ELEMENT = 99;
-		//	const int INDEX_ELEMENT = 1;
-		//	DynamicArray tableau;
+		TEST_METHOD(setElement_devrait_initialiser_un_element_du_dynamicArray)
+		{
+			//Arrange
+			const int NOUVELLE_VALEUR_ELEMENT = 99;
+			const int INDEX_ELEMENT = 1;
+			DynamicArray tableau;
 
-		//	//Action
-		//	tableau.setElement(INDEX_ELEMENT, NOUVELLE_VALEUR_ELEMENT);
+			//Action
+			tableau.setElement(INDEX_ELEMENT, NOUVELLE_VALEUR_ELEMENT);
 
-		//	//Assert
-		//	Assert::AreEqual(NOUVELLE_VALEUR_ELEMENT, tableau.getElement(INDEX_ELEMENT));
-		//}
+			//Assert
+			Assert::AreEqual(NOUVELLE_VALEUR_ELEMENT, tableau.getElement(INDEX_ELEMENT));
+		}
 
-		//TEST_METHOD(augumenter_la_capacite_d_un_dynamicArray_devrait_preserver_le_contenu_deja_present)
-		//{
-		//	//Arrange
-		//	const int CAPACITE_DEPART = 3;
-		//	const int NOUVELLE_CAPACITE = 5;
+		TEST_METHOD(augumenter_la_capacite_d_un_dynamicArray_devrait_preserver_le_contenu_deja_present)
+		{
+			//Arrange
+			const int CAPACITE_DEPART = 3;
+			const int NOUVELLE_CAPACITE = 5;
 
-		//	DynamicArray tableau(CAPACITE_DEPART);
+			DynamicArray tableau(CAPACITE_DEPART);
 
-		//	for (unsigned int i = 0; i < CAPACITE_DEPART; ++i)
-		//	{
-		//		tableau.setElement(i, i*i);
-		//	}
+			for (unsigned int i = 0; i < CAPACITE_DEPART; ++i)
+			{
+				tableau.setElement(i, i*i);
+			}
 
-		//	//Action
-		//	tableau.setCapacite(NOUVELLE_CAPACITE);
+			//Action
+			tableau.setCapacite(NOUVELLE_CAPACITE);
 
-		//	//Assert
-		//	for (unsigned int i = 0; i < CAPACITE_DEPART; ++i)
-		//	{
-		//		Assert::AreEqual(int(i*i), tableau.getElement(i));
-		//	}
-		//	for (unsigned int i = CAPACITE_DEPART; i < NOUVELLE_CAPACITE; ++i)
-		//	{
-		//		Assert::AreEqual(0, tableau.getElement(i));
-		//	}
-		//}
+			//Assert
+			for (unsigned int i = 0; i < CAPACITE_DEPART; ++i)
+			{
+				Assert::AreEqual(int(i*i), tableau.getElement(i));
+			}
+			for (unsigned int i = CAPACITE_DEPART; i < NOUVELLE_CAPACITE; ++i)
+			{
+				Assert::AreEqual(0, tableau.getElement(i));
+			}
+		}
 
 		//TEST_METHOD(diminuer_la_capacite_d_un_dynamicArray_devrait_modifier_sa_capacite)
 		//{
